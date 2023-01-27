ef7f91d489c620f6cbff616f1c1e59471f7e9771
zacharie page <zachariepag@yahoo.com>
2023-01-27 10:51:09 -0500

    Laboratoire 1 Zacharie changement au css

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         if(have_posts()) : 
             while(have_posts()) : the_post();
             the_title('<h1>',' ! ! !', '</h1>');
-            the_content(); ?>
+            the_content('<p>', '</p>'); ?>
             <hr>
             <?php
         endwhile;
