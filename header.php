98f061edc6fbcc83bdecd2a3241b921c293b5c04
zacharie page <zachariepag@yahoo.com>
2023-02-06 12:53:29 -0500

    s3c1 Formatage du menu, creation du fichier scss menu, creation de la branche exer1

--- header.php
+++ header.php
@@ -11,5 +11,5 @@
     <header class="headerSite">
         <h1> <a href="<?php echo bloginfo('url') ?>"><?php echo bloginfo('name') ?></a></h1>
         <h2><?php echo bloginfo('description') ?>-</h2>
-        <p class="menuWp"><?php wp_nav_menu(array("menu" => "entete" )) ?> </p>
+        <p class="menuWp"><?php wp_nav_menu(array("menu" => "entete", "container" => "nav" )) ?> </p>
     </header>
