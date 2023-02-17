f9777b49c073b01ab12244574387188e0b936214
zacharie page <zachariepag@yahoo.com>
2023-02-17 09:55:08 -0500

    s4c2 integration de categorie et de menu

--- category.php
+++ category.php
@@ -1,13 +1,13 @@
 <?php 
 /**
- * Modèle par défaut
+ * Modèle categorie par défaut
  * 
  */
 ?>
 <?php get_header(); ?>
 <main>
 <h2 class="desc"><?php echo bloginfo('description') ?>-</h2>
-    <h2 class="first">front-page.php</h2>
+    <h3 class="first">category.php</h3>
     <section class="blockflex">
     <?php 
         if(have_posts()) : 
@@ -16,7 +16,6 @@
                 <h1 class="post_title">
                     <a href="<?php echo get_permalink();?>"><?php echo get_the_title(); ?></a>
                 </h1>
-                <p><?php if(is_category('cour-du-tim')) echo "cours"; ?></p>
                 <p class="contentWp">
                 <?= wp_trim_words(get_the_excerpt(),10,"&#10148"); ?>           
                 </p>
