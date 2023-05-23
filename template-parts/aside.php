9867c2bef51027a8de6ed48c2f2228e63d8defdf
ZachariePage <zachariepag@yahoo.com>
2023-05-23 10:00:22 -0400

    Formatage du aside

--- template-parts/aside.php
+++ template-parts/aside.php
@@ -1,17 +1,31 @@
 <aside class="site__aside">
+    <?php if(!is_page('')){?>
     <h3 class="menuSecondaire">Menu secondaire</h3>
+    <?php } ?>
     <?php 
     $ma_categorie  = "4w4";
     if (in_category('cours')){
         $ma_categorie  = "cours";
     }
     else if(is_page('')){
+        ?><h3 class="menuSecondaire">Nos atelier</h3><?php
         $ma_categorie = "menuExamAside";
+        ?><div class="imgContainer">
+            <?php
+            the_post_thumbnail('thumbnail');?>
+        </div><?php
+        wp_nav_menu(array(
+            "menu" => $ma_categorie,
+            "container" => "nav"  
+        )); 
     }
-    wp_nav_menu(array(
-        "menu" => $ma_categorie,
-        "container" => "nav"  
-    )); ?>
+    if(!is_page('')){
+        
+        wp_nav_menu(array(
+            "menu" => $ma_categorie,
+            "container" => "nav"  
+        )); 
+    }?>
     <div class="lapinMenus"><img src="rabbit.svg" alt=""></div>
     
     <div class="lapinMenu"></div>
